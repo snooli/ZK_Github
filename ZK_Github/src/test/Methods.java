463b35df35a5b0e2f426c2461b202b185875b9eb
Snorre <olimbano@gmail.com>
2012-07-24 12:07:22 +0200

    Third commit!

--- ZK_Github/src/test/Methods.java
+++ ZK_Github/src/test/Methods.java
@@ -1,5 +1,19 @@
 package test;
 
-public class Methods {
+import org.zkoss.zhtml.Messagebox;
+import org.zkoss.zk.ui.util.GenericForwardComposer;
+import org.zkoss.zul.Textbox;
+import org.zkoss.zul.Window;
 
+
+public class Methods extends GenericForwardComposer {
+	Textbox txtUsername;
+	Textbox txtPassword;
+	Window wnd;
+
+
+  	public void onClick$btnSubmit() throws InterruptedException	{
+  		
+  		Messagebox.show("Brukernavnet ditt er "+ txtUsername.getText() + " og passordet er " + txtPassword.getText());
+  	}
 }
